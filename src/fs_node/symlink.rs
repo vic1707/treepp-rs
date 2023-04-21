0d6a393f4fd2d0af5ec47679c84a88073c757933
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-04-21 12:51:44 +0200

    feat: add `modified_date` to `Symboliclink`

--- src/fs_node/symlink.rs
+++ src/fs_node/symlink.rs
@@ -1,5 +1,6 @@
 use core::result::Result;
 use std::{fs, path::PathBuf};
+use time::OffsetDateTime;
 
 use super::FSNodeError;
 
@@ -8,18 +9,30 @@ pub struct SymbolicLink {
   path: PathBuf,
   target: PathBuf,
   size: i128,
+  modified_date: OffsetDateTime,
 }
 
 impl SymbolicLink {
   pub fn build(path: PathBuf) -> Result<Self, FSNodeError> {
+    let metadata = path
+      .symlink_metadata()
+      .map_err(|err| FSNodeError::metadata(path.clone(), &err))?;
+
     let target = fs::read_link(&path)
       .map_err(|err| FSNodeError::read_link(path.clone(), &err))?;
-    let size = path
-      .symlink_metadata()
-      .map_err(|err| FSNodeError::metadata(path.clone(), &err))?
-      .len()
+
+    let size = metadata.len().into();
+    let modified_date = metadata
+      .modified()
+      .map_err(|err| FSNodeError::modified(path.clone(), &err))?
       .into();
-    Ok(Self { path, target, size })
+
+    Ok(Self {
+      path,
+      target,
+      size,
+      modified_date,
+    })
   }
 
   pub const fn size(&self) -> &i128 {
@@ -32,5 +45,9 @@ impl SymbolicLink {
 
   pub const fn target(&self) -> &PathBuf {
     &self.target
+  }
+
+  pub const fn modified_date(&self) -> &OffsetDateTime {
+    &self.modified_date
   }
 }
