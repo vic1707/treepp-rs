06ab332f78ac41e041f5eb2ec71c9a0abce37f54
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-04-25 18:39:49 +0200

    improvements: sorting algos

--- src/sorters/methods.rs
+++ src/sorters/methods.rs
@@ -1,30 +1,20 @@
 /* Built in imports */
 use core::cmp;
 /* Crate imports */
-use crate::fs_node::FSNodeRes;
+use crate::fs_node::{FSNode, FSNodeRes};
 
 // TODO: write tests for these functions
 
 pub fn name(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
-  let path1 = node1.as_ref().map_or_else(|_| None, |n| Some(n.path()));
-  let path2 = node2.as_ref().map_or_else(|_| None, |n| Some(n.path()));
-  match (path1, path2) {
-    (Some(p1), Some(p2)) => p1.cmp(p2),
-    (Some(_), None) => cmp::Ordering::Less,
-    (None, Some(_)) => cmp::Ordering::Greater,
-    (None, None) => cmp::Ordering::Equal,
-  }
+  let path1 = node1.as_ref().ok().map(FSNode::path);
+  let path2 = node2.as_ref().ok().map(FSNode::path);
+  path1.cmp(&path2)
 }
 
 pub fn size(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
-  let size1 = node1.as_ref().map_or_else(|_| None, |n| Some(n.size()));
-  let size2 = node2.as_ref().map_or_else(|_| None, |n| Some(n.size()));
-  match (size1, size2) {
-    (Some(s1), Some(s2)) => s1.cmp(s2),
-    (Some(_), None) => cmp::Ordering::Less,
-    (None, Some(_)) => cmp::Ordering::Greater,
-    (None, None) => cmp::Ordering::Equal,
-  }
+  let size1 = node1.as_ref().ok().map(FSNode::size);
+  let size2 = node2.as_ref().ok().map(FSNode::size);
+  size1.cmp(&size2)
 }
 
 pub fn extension(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
@@ -34,25 +24,11 @@ pub fn extension(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
   let ext2 = node2
     .as_ref()
     .map_or_else(|_| None, |n| n.path().extension());
-  match (ext1, ext2) {
-    (Some(e1), Some(e2)) => e1.cmp(e2),
-    (Some(_), None) => cmp::Ordering::Less,
-    (None, Some(_)) => cmp::Ordering::Greater,
-    (None, None) => cmp::Ordering::Equal,
-  }
+  ext1.cmp(&ext2)
 }
 
 pub fn modified_date(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
-  let date1 = node1
-    .as_ref()
-    .map_or_else(|_| None, |n| Some(n.modified_date()));
-  let date2 = node2
-    .as_ref()
-    .map_or_else(|_| None, |n| Some(n.modified_date()));
-  match (date1, date2) {
-    (Some(d1), Some(d2)) => d1.cmp(d2),
-    (Some(_), None) => cmp::Ordering::Less,
-    (None, Some(_)) => cmp::Ordering::Greater,
-    (None, None) => cmp::Ordering::Equal,
-  }
+  let date1 = node1.as_ref().ok().map(FSNode::modified_date);
+  let date2 = node2.as_ref().ok().map(FSNode::modified_date);
+  date1.cmp(&date2)
 }
