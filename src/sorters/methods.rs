cd838d90f1cf62e02a7a9d3d4a79d1210f95393f
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-04-25 18:25:02 +0200

    fix: remove useless comparisons

--- src/sorters/methods.rs
+++ src/sorters/methods.rs
@@ -9,7 +9,7 @@ pub fn name(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
   let path1 = node1.as_ref().map_or_else(|_| None, |n| Some(n.path()));
   let path2 = node2.as_ref().map_or_else(|_| None, |n| Some(n.path()));
   match (path1, path2) {
-    (Some(p1), Some(p2)) => p1.cmp(p2).then(p1.cmp(p2)),
+    (Some(p1), Some(p2)) => p1.cmp(p2),
     (Some(_), None) => cmp::Ordering::Less,
     (None, Some(_)) => cmp::Ordering::Greater,
     (None, None) => cmp::Ordering::Equal,
@@ -20,7 +20,7 @@ pub fn size(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
   let size1 = node1.as_ref().map_or_else(|_| None, |n| Some(n.size()));
   let size2 = node2.as_ref().map_or_else(|_| None, |n| Some(n.size()));
   match (size1, size2) {
-    (Some(s1), Some(s2)) => s1.cmp(s2).then(s1.cmp(s2)),
+    (Some(s1), Some(s2)) => s1.cmp(s2),
     (Some(_), None) => cmp::Ordering::Less,
     (None, Some(_)) => cmp::Ordering::Greater,
     (None, None) => cmp::Ordering::Equal,
@@ -35,7 +35,7 @@ pub fn extension(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
     .as_ref()
     .map_or_else(|_| None, |n| n.path().extension());
   match (ext1, ext2) {
-    (Some(e1), Some(e2)) => e1.cmp(e2).then(e1.cmp(e2)),
+    (Some(e1), Some(e2)) => e1.cmp(e2),
     (Some(_), None) => cmp::Ordering::Less,
     (None, Some(_)) => cmp::Ordering::Greater,
     (None, None) => cmp::Ordering::Equal,
@@ -50,7 +50,7 @@ pub fn modified_date(node1: &FSNodeRes, node2: &FSNodeRes) -> cmp::Ordering {
     .as_ref()
     .map_or_else(|_| None, |n| Some(n.modified_date()));
   match (date1, date2) {
-    (Some(d1), Some(d2)) => d1.cmp(d2).then(d1.cmp(d2)),
+    (Some(d1), Some(d2)) => d1.cmp(d2),
     (Some(_), None) => cmp::Ordering::Less,
     (None, Some(_)) => cmp::Ordering::Greater,
     (None, None) => cmp::Ordering::Equal,
