a89ac435da6cfe8eebcf7c400d34d14bb2b117f0
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-06-21 11:12:52 +0200

    cmd: `cargo fmt`

--- src/formatters/name_only.rs
+++ src/formatters/name_only.rs
@@ -12,10 +12,14 @@ impl FormatterT for NameOnly {
   }
 
   fn format_file(&self, file: &File) -> String {
-  file.filename.clone()
+    file.filename.clone()
   }
 
   fn format_symlink(&self, symlink: &Symlink) -> String {
-    format!("{} -> {}",symlink.filename.clone(), symlink.target_filename.clone())
+    format!(
+      "{} -> {}",
+      symlink.filename.clone(),
+      symlink.target_filename.clone()
+    )
   }
 }
